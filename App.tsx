15e6d176d0fc306b32afb93beec3ae9bd0f2ca4f
Роман Круглянский <romankruglanskij@Noutbuk-Roman.local>
2025-07-24 10:13:19 +0300

    ref App

--- App.tsx
+++ App.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView, StyleSheet } from 'react-native';
 
 import StepCounter from './src/components/StepCounter';
 
 function App(): React.JSX.Element {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       <StepCounter />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
